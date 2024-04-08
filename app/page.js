ba04fe08a6eebaf6e790a3598c5335cfe0215e25
Anas <anasolin01777@gmail.com>
2024-04-09 00:18:14 +0600

    git init

---
+++ app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">SM Anas Working In This site</div>
+  );
+}
